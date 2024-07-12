8f244a3cb094a3b8a9b4808fd90178f34895fe8c
Mihir2103 <mihirbondre788142gmail.com>
2024-07-12 16:33:49 +0530

    Commit -1 -Action

---
+++ webapp/firstapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class insurance(models.Model):
+    age = models.FloatField()
+    sex = models.FloatField()
+    bmi = models.FloatField()
+    children = models.FloatField()
+    smoker = models.FloatField()
+    region = models.FloatField()
+    expenses= models.FloatField()
+    
+
